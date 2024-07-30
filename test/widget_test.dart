41bccddfda386aeaeacc70f31b32575b88885ccd
ASLIDDINCODER <147050428+ASLIDDINCODERn1@users.noreply.github.com>
2024-07-30 19:15:42 +0500

    uptdates

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,15 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:lottie/lottie.dart';
 
-import 'package:quiz_app/main.dart';
 import 'package:quiz_app/views/screens/quiz_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MaterialApp(
+    await tester.pumpWidget(const MaterialApp(
       home: QuizScreen(),
     ));
 
